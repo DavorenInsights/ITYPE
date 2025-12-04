1e01fc55e991bbb40bc1725001febb5524b931ff
DavorenInsights <davoreninsights@gmail.com>
2025-12-04 17:08:45 +0200

    Update data_logger.py

--- data_logger.py
+++ data_logger.py
@@ -38,12 +38,4 @@ def log_to_google_sheets(final_archetype, stability, shadow, scores, raw_answers
     ]
 
     sheet.append_row(row)
-import traceback
-import streamlit as st
 
-try:
-    worksheet.append_row(row)
-    st.write("Logged successfully")
-except Exception as e:
-    st.error("Logging failed")
-    st.code(traceback.format_exc())
